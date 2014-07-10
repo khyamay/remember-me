e08364e91ccbb35de48698a3484f2e4f25d780f6
Khemraj Thapa <khyamay@gmail.com>
2014-07-11 00:05:13 +0100

    added contorller for each views

---
+++ www/js/controllers.js
@@ -0,0 +1,13 @@
+angular.module('mainApp', [])
+	.controller('homeCtr', function(){
+
+	})
+	.controller('notesCtrl', function(){
+
+	})
+	.controller('picturesCtrl', function(){
+
+	})
+	.controller('messagesCtrl', function(){
+		
+	})
