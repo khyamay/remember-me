3c48f630a3b0b747117135382cb99d38bcf5c72f
Khemraj Thapa <khyamay@gmail.com>
2014-07-11 00:02:55 +0100

    added views of each templates

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('mainApp', ['ionic'])
+angular.module('mainApp', ['ionic', 'service', 'controllers'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, service, controllers) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -21,7 +21,18 @@ angular.module('mainApp', ['ionic'])
   $stateProvider
     .state('home', {
       url: '/home',
-      abstratc: true,
       templateUrl: 'templates/home.html'
+    })
+    .state('notes', {
+      url: '/notes', 
+        templateUrl: 'templates/notes.html'
+    })
+    .state('pictures', {
+      url: '/pictures',
+      templateUrl: 'templates/pictures.html'
+    }).
+    state('messages', {
+      url: '/messages',
+      templateUrl: 'templates/messages.html'
     })
 })
