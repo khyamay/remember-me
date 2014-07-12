70506a7d256e49086c46da6beab9b9805a005912
Khemraj Thapa <khyamay@gmail.com>
2014-07-12 01:49:59 +0100

    added services on messageCtrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,6 +8,6 @@ angular.module('mainApp.controllers', [])
 	.controller('picturesCtrl', function($scope){
 
 	})
-	.controller('messagesCtrl', function($scope){
-		
+	.controller('messagesCtrl', function($scope, Messages){
+		$scope.messages = Messages.all();
 	})
