888c819a5bca9ff42068ca0acce4923b4abf37a5
Khemraj Thapa <khyamay@gmail.com>
2014-07-14 23:14:31 +0100

    added new notes ctrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,19 +1,26 @@
 angular.module('mainApp.controllers', [])
 	.controller('homeCtrl', function($scope){
-
+		
 	})
-	.controller('notesCtrl', function($scope, $ionicModal, $ionicActionSheet){
+	.controller('notesCtrl', function($scope, $ionicModal){
+		
 		$ionicModal.fromTemplateUrl('templates/newNotes.html', function (modal){
-			$scope.settingsModal = modal; 
+			$scope.settingsModal = modal;
 		});
-
 		$scope.newNotes = function (){
 			$scope.settingsModal.show();
-		}
+		};
 	})
 	.controller('picturesCtrl', function($scope){
 
 	})
 	.controller('messagesCtrl', function($scope, Messages){
 		$scope.messages = Messages.all();
+	})
+	.controller('newNotesCtrl', function($scope, $ionicModal){
+
+		$scope.close = function (modal){
+			$scope.modal.hide();
+		}
+		
 	})
