c42eaf21af113cacd532edcae2f722268d4f8a00
Khemraj Thapa <khyamay@gmail.com>
2014-08-12 23:04:12 +0100

    removed factory for messages

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,16 @@
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes.dex : \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard$1.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard$2.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/com/ionicframework/starter/BuildConfig.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/com/ionicframework/starter/R$attr.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/com/ionicframework/starter/R$drawable.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/com/ionicframework/starter/R$string.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/com/ionicframework/starter/R$xml.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/com/ionicframework/starter/R.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/com/ionicframework/starter/RememberMe.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/org/apache/cordova/camera/CameraLauncher.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/org/apache/cordova/camera/ExifHelper.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/org/apache/cordova/camera/FileHelper.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/classes/org/apache/cordova/device/Device.class \
+/Users/khyamay/desktop/sideprojects/remember-me/platforms/android/ant-build/dexedLibs/classes-5be5d5dc4b38042f77d4f287022a82b3.jar \
