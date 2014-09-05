f6d398c601fc7d820bc752af556c052cbb7d2120
Khemraj Thapa <khyamay@gmail.com>
2014-09-05 10:57:04 +0100

    Added android platform + app icon

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/khyamay/Desktop/SideProjects/remember-me/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/khyamay/Desktop/SideProjects/remember-me/platforms/android/CordovaLib/AndroidManifest.xml \
